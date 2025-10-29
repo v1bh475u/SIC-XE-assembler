d7530750af71026d29fb62001b2f8a3fe6acd85d
vibhatsu <maulikbarot2915@gmail.com>
2025-10-29 20:03:35 +0530

    feat(main): add main function to initialize assembler and handle command line arguments
    Signed-off-by: vibhatsu <maulikbarot2915@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "assembler.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " <input_file> <output_file>\n";
+    return 1;
+  }
+
+  sicxe::Assembler assembler;
+  assembler.assemble(argv[1], argv[2]);
+  return 0;
+}
